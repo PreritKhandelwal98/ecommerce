00953eaf6e10bc546c18613613c9194ee3e6ca35
Prerit Khandelwal <prerit.web@gmail.com>
2024-03-12 16:24:55 +0530

    navbar partially completed

--- ecommerce/src/Components/header/Header.js
+++ ecommerce/src/Components/header/Header.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './Header.module.scss'
 
 import { Link } from 'react-router-dom';
-import { FaShoppingCart } from 'react-icons/fa'
+import { FaShoppingCart, FaTimes } from 'react-icons/fa'
 import { HiOutlineMenuAlt3 } from 'react-icons/hi'
+
+
 const logo = (
     <div className={styles.logo}>
         <Link to="/">
@@ -22,12 +24,32 @@ const cart = (
     </span>
 )
 function Header() {
+    const [showMenu, setShowMenu] = useState(false);
+
+    const toggleMenu = () => {
+        setShowMenu(!showMenu)
+    };
+
+    const hideMenu = () => {
+        setShowMenu(false);
+    };
+
+
     return (
         <header>
             <div className={styles.header}>
                 {logo}
-                <nav>
-                    <ul>
+                <nav className={showMenu ? `${styles['show-nav']}` : `${styles['hide-nav']}`}>
+                    <div className={showMenu ? `${styles["nav-wrapper"]} ${styles['show-nav-wrapper']}` : `${styles['nav-wrapper']}`} onClick={hideMenu}>
+                    </div>
+
+                    <ul onClick={hideMenu}>
+                        <li className={styles["logo-mobile"]}>
+                            <Link to="/">
+                                {logo}
+                                <FaTimes size={22} color="#fff" onClick={hideMenu} />
+                            </Link>
+                        </li>
                         <li>
                             <Link to="/">
                                 Home
@@ -39,7 +61,7 @@ function Header() {
                             </Link>
                         </li>
                     </ul>
-                    <div className={styles["header-right"]}>
+                    <div className={styles["header-right"]} onClick={hideMenu}>
                         <span className={styles.links}>
                             <Link to="/login">Login</Link>
                             <Link to="/register">Register</Link>
@@ -47,11 +69,12 @@ function Header() {
                         </span>
                         {cart}
                     </div>
+
                 </nav>
 
                 <div className={styles["menu-icon"]}>
                     {cart}
-                    <HiOutlineMenuAlt3 size={28} />
+                    <HiOutlineMenuAlt3 size={28} onClick={toggleMenu} />
                 </div>
             </div>
         </header>
