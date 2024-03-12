76c3a081dd60a39d80520baa06b9c8b9a101f5ec
Prerit Khandelwal <prerit.web@gmail.com>
2024-03-12 20:22:49 +0530

    login and signup logic completed

--- ecommerce/src/Pages/auth/Login.js
+++ ecommerce/src/Pages/auth/Login.js
@@ -1,12 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react';
 import styles from './auth.module.scss'
 import loginImg from '../../assets/login.png'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { FaGoogle } from 'react-icons/fa'
 import Card from '../../Components/card/Card'
-function login() {
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../../Firebase/confing'
+import { ToastContainer, toast } from 'react-toastify';
+import Loader from '../../Components/loader/Loader'
+
+function Login() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate();
+    const [isLoading, setLoading] = useState(false);
+
+    const loginUser = (e) => {
+        e.preventDefault();
+        isLoading(true);
+        signInWithEmailAndPassword(auth, email, password)
+            .then(() => {
+                isLoading(false);
+                toast.success("User Loggedin");
+                setEmail('');
+                setPassword('');
+                navigate("/")
+            })
+            .catch(error => {
+                toast.error(error.message);
+                isLoading(false);
+            });
+    }
     return (
         <>
+
+            <ToastContainer />
+            {isLoading && <Loader />}
             <section className={`container ${styles.auth}`}>
                 <div className={styles.img}>
                     <img src={loginImg} alt="Login" width="400" />
@@ -15,19 +44,19 @@ function login() {
                     <div className={styles.form}>
                         <h2 >Login</h2>
 
-                        <form >
-                            <input type="text" value="" placeholder='Email' required />
-                            <input type="password" value="" placeholder='Password' required />
+                        <form onSubmit={loginUser}>
+                            <input type="text" placeholder='Email' required onChange={event => setEmail(event.target.value)} value={email} />
+                            <input type="password" placeholder='Password' required onChange={event => setPassword(event.target.value)} value={password} />
                             <button className="--btn --btn-primary --btn-block">Login</button>
                             <div className={styles.links}>
                                 <Link to='/reset'>Forgot Password</Link>
                             </div>
                             <p>-- or --</p>
                         </form>
-                        <button className="--btn --btn-danger --btn-block"><FaGoogle color="#fff" /> Login with Google</button>
+                        <button className="--btn --btn-danger --btn-block" type="submit"><FaGoogle color="#fff" /> Login with Google</button>
                         <span className={styles.register}>
-                            <p>Don't have Account  </p>
-                            <Link to="/register">Register</Link>
+                            <p>Don't have Account?  </p>
+                            <Link to="/register"><u>Register</u></Link>
                         </span>
                     </div>
                 </Card>
@@ -36,4 +65,4 @@ function login() {
     )
 }
 
-export default login
+export default Login
--- ecommerce/src/Pages/auth/Register.js
+++ ecommerce/src/Pages/auth/Register.js
@@ -1,29 +1,61 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, Navigate, useNavigate } from 'react-router-dom'
 import styles from './auth.module.scss'
 import registerImg from '../../assets/register.png'
 import Card from '../../Components/card/Card'
-
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import Loader from '../../Components/loader/Loader'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { auth } from '../../Firebase/confing'
 function Register() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [cpassword, setCPassword] = useState('');
+    const [isLoading, setLoading] = useState(false);
+    const navigate = useNavigate();
+    const createUser = (e) => {
+        e.preventDefault();
+        if (password !== cpassword) {
+            toast.error("Password do not match.");
+        }
+        setLoading(true)
+        createUserWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                const user = userCredential.user;
+                console.log(user);
+                setLoading(false);
+                toast.success("User Registered Successfully");
+                setEmail('');
+                setPassword('');
+                navigate("/login")
+            })
+            .catch(error => {
+                toast.error(error.message);
+                setLoading(false);
+            });
+    }
     return (
         <>
+            <ToastContainer />
+            {isLoading && <Loader />}
             <section className={`container ${styles.auth}`}>
 
                 <Card>
                     <div className={styles.form}>
                         <h2 >Register</h2>
 
-                        <form >
-                            <input type="text" value="" placeholder='Email' required />
-                            <input type="password" value="" placeholder='Password' required />
-                            <input type="password" value="" placeholder='Confirm Password' required />
-                            <button className="--btn --btn-primary --btn-block">Register</button>
+                        <form onSubmit={createUser}>
+                            <input type="text" placeholder='Email' required onChange={event => setEmail(event.target.value)} value={email} />
+                            <input type="password" placeholder='Password' required onChange={event => setPassword(event.target.value)} value={password} />
+                            <input type="password" placeholder='Confirm Password' required onChange={event => setCPassword(event.target.value)} value={cpassword} />
+                            <button className="--btn --btn-primary --btn-block" type="submit">Register</button>
                             <div className={styles.links}>
                             </div>
                         </form>
                         <span className={styles.register}>
                             <p>Already have Account?  </p>
-                            <Link to="/login">Login</Link>
+                            <Link to="/login"><u>Login</u></Link>
                         </span>
                     </div>
                 </Card>
